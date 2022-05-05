6cde8ad113980ee0afc61b3904d3e1a8e61c3332
cizquierdo <cizquierdo@everycode.es>
2022-05-05 09:19:45 +0200

    Mejoras interfaz tabla

--- src/components/TableView.vue
+++ src/components/TableView.vue
@@ -1,45 +1,71 @@
 <template>
   <div class="container">
-<div class="row" >
-    <table class="table table-dark table-striped table-sm" >
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">url</th>         
-          <th scope="col"><button class="btn btn-primary btn-sm" @click="store.orden()" title="Ordena la muestra por tipo de página">Tipo</button> </th>
-          <th scope="col">Shortname</th>
-          <th scope="col">Migas</th>
-          <th scope="col">acción</th>
-        </tr>
-      </thead>
+    <div class="row">
+      <table class="table table-dark table-striped table-sm">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th>
+              <span
+                aria-label="Despliega"
+                class="fa-solid fa-up-right-from-square text-primary boton"
+                title="Despliega todo el sitio en pestañas (No funciona en Chrome)"
+                @click="store.openUrlList()"
+              ></span>
+            </th>
+            <th scope="col">url</th>
+            <th scope="col">
+              <button
+                class="btn btn-primary btn-sm"
+                @click="store.orden()"
+                title="Ordena la muestra por tipo de página"
+              >
+                Tipo
+              </button>
+            </th>
+            <th scope="col">Shortname</th>
+            <th scope="col">Migas</th>
+            <th scope="col">acción</th>
+          </tr>
+        </thead>
 
-      <tbody>
-        <tr 
-        v-for="(page,index) in store.siteWebPages">
-          <td>{{index}}</td>
-          <td 
-            class="table-text-wrap "
-            id="urlCell"
-            @click.ctrl="store.openUrl(page.webPageUrl)"
-            title="ctrl+click para abrir">
-              {{page.webPageUrl}}
+        <tbody>
+          <tr v-for="(page, index) in store.siteWebPages">
+            <td>{{ index }}</td>
+            <td @click="store.openUrl(page.webPageUrl)" class="boton">
+              <span
+                title="Abre en nueva pestaña"
+                class="fa-solid fa-up-right-from-square text-warning"
+              ></span>
             </td>
-          <td class="table-text-wrap">{{page.webPageType}}</td>
-          <td>{{page.shortName}}</td>
-          <td class="table-text-wrap">{{page.breadcrumb}}</td>
-          <td>
-            <button class="btn btn-danger btn-sm" @click="store.deleteItem(index)">
-              <span class="fa fa-trash fa-xs" aria-label="Borra línea"></span>
-            </button>
-            <button class="btn btn-warning btn-sm" @click="store.editItem(index)">
-              <span class="fa fa-edit fa-xs" aria-label="Edita línea"></span>
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-</div>
-
+            <td
+              class="table-text-wrap"
+              @click.ctrl="store.openUrl(page.webPageUrl)"
+              title="ctrl+click para abrir en nueva pestaña"
+            >
+              {{ page.webPageUrl }}
+            </td>
+            <td class="table-text-wrap">{{ page.webPageType }}</td>
+            <td>{{ page.shortName }}</td>
+            <td class="table-text-wrap">{{ page.breadcrumb }}</td>
+            <td>
+              <button
+                class="btn btn-danger btn-sm"
+                @click="store.deleteItem(index)"
+              >
+                <span class="fa fa-trash fa-xs" aria-label="Borra línea"></span>
+              </button>
+              <button
+                class="btn btn-warning btn-sm"
+                @click="store.editItem(index)"
+              >
+                <span class="fa fa-edit fa-xs" aria-label="Edita línea"></span>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -57,37 +83,41 @@ export default {
 };
 </script>
 <style scoped>
-table{
+table {
   min-width: 100%;
   table-layout: fixed;
 }
-thead th:nth-child(1){
-  width:3%;
+thead th:nth-child(1) {
+  width: 2%;
 }
-thead th:nth-child(2){
-  width:40%;
+thead th:nth-child(2) {
+  width: 2%;
 }
-thead th:nth-child(3){
-  width:12%;
+
+thead th:nth-child(3) {
+  width: 40%;
 }
-thead th:nth-child(4){
-  width:15%;
+thead th:nth-child(4) {
+  width: 11%;
 }
-thead th:nth-child(5){
-  width:26%;
+thead th:nth-child(5) {
+  width: 15%;
 }
-thead th:nth-child(6){
-  width:7%;
+thead th:nth-child(6) {
+  width: 24%;
 }
-.table-text{
+thead th:nth-child(7) {
+  width: 6%;
+}
+.table-text {
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
 }
-.table-text-wrap{
+.table-text-wrap {
   word-wrap: break-word;
 }
-#urlCell{
+.boton {
   cursor: pointer;
 }
 </style>
